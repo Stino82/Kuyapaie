71224990307d1f574d48ceef5622bdf34d91a006
Stino82 <celestinmayombo214@gmail.com>
2026-04-08 01:20:31 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import numpy as np
 import pandas as pd
-from scipy.stats import norm
+import scipy.stats norm
 import yfinance as yf
 import streamlit.components.v1 as components
 
